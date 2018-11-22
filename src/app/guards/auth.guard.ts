82bbc5545a769bfd99f51ff8cd1ea988818d93e6
Divesh Panwar <divesh.panwar@gmail.com>
2018-11-23 00:03:37 +0530

    cart url fix in navigation

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -16,8 +16,10 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
+    console.log('I am guarding');
+
     const loggedIn = window.sessionStorage.getItem('loggedIn');
-    if (loggedIn === 'true') {
+    if (loggedIn == 'true') {
       return true;
     } else {
       this.router.navigate(['/login']);
